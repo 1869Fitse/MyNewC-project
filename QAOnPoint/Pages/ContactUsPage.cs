08bfa0ddfc302477fb9f15f64aa2ec95b5280706
Fitsum <fitseandu@gmail.com>
2022-02-13 16:37:39 -0500

    added page object

---
+++ QAOnPoint/Pages/ContactUsPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using QAOnPoint;
+
+namespace ChelseaFcProject.Pages
+{
+    public class ContactUsPage
+    {
+        Program program = new Program();
+
+
+        
+        public ContactUsPage()
+        {
+            program.TestMethod();
+
+        }
+    }
+}
---
+++ QAOnPoint/Pages/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace QAOnPoint
+{
+
+    public class Program
+    {
+
+        IWebDriver Driver;
+        WebDriverWait wait;
+
+    
+        [SetUp]
+        public void Initialize()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            Driver = new ChromeDriver();
+            wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+            
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Driver.Quit();
+        }
+
+
+        [Test]
+        public void TestMethod()
+        {
+        //https://abstracta.us
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("https://statsroyale.com/");
+            Driver.FindElement(By.XPath("//a[@class= 'ui__link ui__whiteText ui__mediumText footer__link' and text()= 'Cards']")).Click();
+            Thread.Sleep(5000);
+
+           var iceSpirit = Driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']"));
+           Assert.True(iceSpirit.Displayed);
+
+        }
+    }
+}
