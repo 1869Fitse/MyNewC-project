c77e14f4a1c316b48010565fe3d10a1e3f7d60e9
Fitsum <fitseandu@gmail.com>
2022-02-15 02:01:50 -0500

    Added Explicit wait method

--- QAOnPoint/Pages/Program.cs
+++ QAOnPoint/Pages/Program.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
+
 namespace QAOnPoint
 {
 
@@ -13,19 +14,20 @@ namespace QAOnPoint
 
         IWebDriver Driver;
         WebDriverWait wait;
-
-    
+       
         [SetUp]
         public void Initialize()
         {
             var options = new ChromeOptions();
-            options.AddArgument("--start-maximized");
+            //options.AddArgument("--start-maximized");
             Driver = new ChromeDriver();
-            wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
-            
+            //wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+            //Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+
         }
 
-        
+
         [TearDown]
         public void Cleanup()
         {
@@ -39,13 +41,21 @@ namespace QAOnPoint
         
             Driver.Manage().Window.Maximize();
             Driver.Navigate().GoToUrl("https://statsroyale.com/");
+
+            waitForElement(Driver, By.XPath("//a[@class= 'ui__link ui__whiteText ui__mediumText footer__link' and text()= 'Cards']") ,5);
+
             Driver.FindElement(By.XPath("//a[@class= 'ui__link ui__whiteText ui__mediumText footer__link' and text()= 'Cards']")).Click();
-            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             var iceSpirit = Driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']"));
             Assert.True(iceSpirit.Displayed);
             iceSpirit.Click();
-            //Added thread.sleep for 5 minutes delay time
-            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void waitForElement(IWebDriver Driver,By element,int timewait) {
+
+            wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timewait));
+            wait.Until(e => e.FindElement(element));
+                        
         }
     }
-}
+    }
+
