db4cc25372643abc5af62c614f90b1b518d06270
Fitsum <fitseandu@gmail.com>
2022-02-17 00:29:03 -0500

    updated code

--- QAOnPoint/Pages/Program.cs
+++ QAOnPoint/Pages/Program.cs
@@ -6,7 +6,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
 
-namespace QAOnPoint
+namespace ChelseaFcProject.Pages
 {
 
     public class Program
@@ -14,7 +14,9 @@ namespace QAOnPoint
 
         IWebDriver Driver;
         WebDriverWait wait;
-       
+
+
+
         [SetUp]
         public void Initialize()
         {
@@ -38,24 +40,26 @@ namespace QAOnPoint
         [Test]
         public void TestMethod()
         {
-        
+
             Driver.Manage().Window.Maximize();
             Driver.Navigate().GoToUrl("https://statsroyale.com/");
-            IWebElement card = Driver.FindElement(By.XPath("//a[@class= 'ui__link ui__whiteText ui__mediumText footer__link' and text()= 'Cards']"));
-            waitForElementAndClick(Driver, By.XPath("//a[@class= 'ui__link ui__whiteText ui__mediumText footer__link' and text()= 'Cards']"),5, card);
-
-            var iceSpirit = Driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']"));
-            waitForElementAndClick(Driver, By.CssSelector("a[href*='Ice+Spirit']"), 5, iceSpirit);
+            IWebElement Card = Driver.FindElement(By.XPath("//a[@class= 'ui__link ui__whiteText ui__mediumText footer__link' and text()= 'Cards']"));
+            WaitForElementAndClick(Driver, By.XPath("//a[@class= 'ui__link ui__whiteText ui__mediumText footer__link' and text()= 'Cards']"), 5, Card);
+            IWebElement iceSpirit = Driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']"));
+            WaitForElementAndClick(Driver, By.CssSelector("a[href*='Ice+Spirit']"), 5, iceSpirit);
             Thread.Sleep(5000);
         }
-        
-        public void waitForElementAndClick(IWebDriver Driver,By element,int timewait, IWebElement webElement) {
+
+        public void WaitForElementAndClick(IWebDriver Driver, By element, int timewait, IWebElement webElement)
+        {
 
             wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timewait));
             wait.Until(e => e.FindElement(element));
             webElement.Click();
-                        
+
         }
+
     }
-    }
+}
+
 
